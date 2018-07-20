8dbab774f895c85e6f335301b5e81709416960dc
RobbieClarken <robbie.clarken@gmail.com>
2018-07-20 19:31:13 +1000

    Fix test by using autospec

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ HEADERS = {"Content-Type": "application/json"}
 
 def send_alert(message):
     payload = {"message": message}
-    response = requests.post(URL, body=json.dumps(payload), headers=HEADERS)
+    response = requests.post(URL, data=json.dumps(payload), headers=HEADERS)
     if not response.ok:
         print(f"alert failed: {response.txt}")
         return False
     return True
 
 
-@mock.patch("requests.post")
+@mock.patch("requests.post", autospec=True)
 def test_successful_alert(mock_post):
     assert send_alert("foo") is True
-    mock_post.assert_called_with(URL, body='{"message": "foo"}', headers=HEADERS)
+    mock_post.assert_called_with(URL, data='{"message": "foo"}', headers=HEADERS)
